9e1b72cff8e334c3302fef91db8f9a4f303bc7a9
Gguigre <gui.gremi@gmail.com>
2019-10-23 20:47:12 +0200

    add miaou

--- bots/belucat.js
+++ bots/belucat.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const path = require("path");
 
 class Belucat {
   constructor(token) {
@@ -10,7 +11,23 @@ class Belucat {
     this.bot.on("message", message => {
       console.log("== Belucat : New message received");
       if (message.content.match(/(.*)belucat(.*)/gi)) {
+        if (message.content.match(/(.*)(dodo)(.*)/gi)) {
+          this.voiceChannel.leave();
+          return message.react("💤");
+        }
+
         console.log("Contains Belucat");
+        this.voiceChannel = message.member.voiceChannel;
+
+        this.voiceChannel
+          .join()
+          .then(connection => {
+            const file = path.resolve(
+              `./asset/miaou${Math.random() < 0.5 ? "" : "2"}.ogg`
+            );
+            const dispatcher = connection.playFile(file);
+          })
+          .catch(console.error);
         message.reply("Mew !");
       }
       if (message.content.match(/(.*) (cat|chat)(.*)/gi)) {
